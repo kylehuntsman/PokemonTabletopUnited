bbd4a781ce5a41e2ee356fe6ce6c7e38d81bf578
JackKell <Jackkell@Citadel>
2014-10-09 23:45:42 -0700

    Updated PokeEdge and Pokemon

--- Pokemon Tabletop United/src/com/ptu/pokemon/Pokemon.java
+++ Pokemon Tabletop United/src/com/ptu/pokemon/Pokemon.java
@@ -20,7 +20,8 @@ public class Pokemon {
 	private String gender;
 	private Nature nature;
 	private AbilityManager abilities;
-	private int overland, swim, longJump, highJump, injuries; 
+	private int overland, swim, longJump, highJump, 
+	injuries, tutorPoint; 
 	private CombatStatManager combatStats;
 	private MoveManager moves;
 	private SkillManager skills;
@@ -28,4 +29,141 @@ public class Pokemon {
 	private List<Vitamin> vitmains; //Currently does nothing
 	private List<Poffin> poffins;   //Currently does nothing
 	private List<PokeEdge> edges;   //Currently does nothing
+	private int currentTutorPoints;
+	private String notes;
+	
+	public Pokemon(Species species, String nickname, Type type1, Type type2,
+			int level, Item heldItem, int loyalty, String gender,
+			Nature nature, AbilityManager abilities, int overland, int swim,
+			int longJump, int highJump, int injuries, int tutorPoint,
+			CombatStatManager combatStats, MoveManager moves,
+			SkillManager skills, ContestStatManager contestStats,
+			List<Vitamin> vitmains, List<Poffin> poffins, List<PokeEdge> edges,
+			int currentTutorPoints, String notes) {
+		this.species = species;
+		this.nickname = nickname;
+		this.type1 = type1;
+		this.type2 = type2;
+		this.level = level;
+		this.heldItem = heldItem;
+		this.loyalty = loyalty;
+		this.gender = gender;
+		this.nature = nature;
+		this.abilities = abilities;
+		this.overland = overland;
+		this.swim = swim;
+		this.longJump = longJump;
+		this.highJump = highJump;
+		this.injuries = injuries;
+		this.tutorPoint = tutorPoint;
+		this.combatStats = combatStats;
+		this.moves = moves;
+		this.skills = skills;
+		this.contestStats = contestStats;
+		this.vitmains = vitmains;
+		this.poffins = poffins;
+		this.edges = edges;
+		this.currentTutorPoints = currentTutorPoints;
+		this.notes = notes;
+	}
+
+	public AbilityManager getAbilities() {
+		return abilities;
+	}
+
+	public CombatStatManager getCombatStats() {
+		return combatStats;
+	}
+
+	public ContestStatManager getContestStats() {
+		return contestStats;
+	}
+
+	public int getCurrentTutorPoints() {
+		return currentTutorPoints;
+	}
+
+	public List<PokeEdge> getEdges() {
+		return edges;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public Item getHeldItem() {
+		return heldItem;
+	}
+
+	public int getHighJump() {
+		return highJump;
+	}
+
+	public int getInjuries() {
+		return injuries;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public int getLongJump() {
+		return longJump;
+	}
+
+	public int getLoyalty() {
+		return loyalty;
+	}
+
+	public MoveManager getMoves() {
+		return moves;
+	}
+
+	public Nature getNature() {
+		return nature;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public int getOverland() {
+		return overland;
+	}
+
+	public List<Poffin> getPoffins() {
+		return poffins;
+	}
+
+	public SkillManager getSkills() {
+		return skills;
+	}
+
+	public Species getSpecies() {
+		return species;
+	}
+
+	public int getSwim() {
+		return swim;
+	}
+
+	public int getTutorPoint() {
+		return tutorPoint;
+	}
+
+	public Type getType1() {
+		return type1;
+	}
+
+	public Type getType2() {
+		return type2;
+	}
+
+	public List<Vitamin> getVitmains() {
+		return vitmains;
+	}
 }
