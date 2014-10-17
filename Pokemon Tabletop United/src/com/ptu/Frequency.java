68b0a2536de8e0f1af81ce9ac1f2ea2e2c806bbf
JackKell <Jackkell@Citadel>
2014-10-17 03:05:19 -0700

    Java Docs for Ability, AbilityManager (which we may remove), CombatStat, CombatStatManager, DiceRoll, and Frequency.

--- Pokemon Tabletop United/src/com/ptu/Frequency.java
+++ Pokemon Tabletop United/src/com/ptu/Frequency.java
@@ -1,13 +1,14 @@
 package com.ptu;
 
+//TODO Finish writting the the java doc for frequency enum and re write what a frequency enum is.
+
+/**
+ * The <code>Frequency</code> enum represents a how often a Feature or Move may
+ * be performed and what kind of action is required to perform them.
+ */
 public enum Frequency {
-	AT_WILL("At-Will"),
-	STATIC("Static"),
-	TIME("Time X"),
-	TIME_PER_X("Time/X"),
-	AP("X AP"),
-	BIND_AP("Bind X AP"),
-	DRAIN_AP("Drain X AP");
+	AT_WILL("At-Will"), STATIC("Static"), TIME("Time X"), TIME_PER_X("Time/X"), AP(
+			"X AP"), BIND_AP("Bind X AP"), DRAIN_AP("Drain X AP");
 
 	private String value;
 
